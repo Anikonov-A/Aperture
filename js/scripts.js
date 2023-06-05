783d539067e8f8f846773c6bb8c43155c8674506
Anton Anikonov <antonanikonov1@gmail.com>
2023-06-05 23:22:04 +0300

    accordion

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,27 @@
-$(document).ready(function(){
+$(document).ready(function () {
     $('.owl-carousel').owlCarousel({
-        loop:true,
-        nav:true,
-        autoplay:true,
-        autoplayTimeout:2000,
-        navText: [ '', ' ' ],
+        loop: true,
+        nav: true,
+        autoplay: true,
+        autoplayTimeout: 2000,
+        navText: ['', ' '],
 
-        responsive:{
-            0:{
-                items:1
+        responsive: {
+            0: {
+                items: 1
             },
-          /*  768:{
-                items:2
-            },*/
-            1100:{
-                items:5
+            /*  768:{
+                  items:2
+              },*/
+            1100: {
+                items: 5
             }
         }
     });
-    $( function() {
-        $( "#accordion" ).accordion();
-    } );
+    $(function () {
+        $("#accordion").accordion({
+            heightStyle:'content'
+        });
+    });
+
 });
